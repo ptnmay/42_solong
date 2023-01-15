0e2c7e39f90c469819f8a7b770105f91c278e51d
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-01-16 00:17:48 +0700

    ..

--- ff_namtuam.c
+++ ff_namtuam.c
@@ -6,17 +6,17 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 03:16:40 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/01/15 00:31:01 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/01/15 21:06:47 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include    "so_long.h"
 
-void    ff_namtuam(t_gm *game,char **mapff int x, int y)
+void    ff_namtuam(t_gm *game, char **mapff, int x, int y)
 {
     if (mapff[x][y] == '1')
         return ;
-    copy[x][y] = '1';
+    game->copy[x][y] = '1';
     ff_namtuam(game, mapff, x, y - 1); //up
     ff_namtuam(game, mapff, x, y + 1); //down
     ff_namtuam(game, mapff, x - 1, y); //left
