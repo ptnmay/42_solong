8fb0d4f7b54caa27f05c072b50ce9b56599ad636
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-01-17 01:20:35 +0700

    .

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 15:12:39 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/01/17 00:15:36 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/01/17 01:15:09 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 # include "./get_next_line/get_next_line_bonus.h"
 # include "./libft/libft.h"
-//# include "./minilibx/mlx.h"
+# include "./minilibx/mlx.h"
 
 typedef struct s_game
 {
